6d99cfcb3aa2c4b0f514cb6e5568bf99bb3897bd
Kaic <kaictacker@gmail.com>
2023-10-13 21:36:07 -0300

    creating spring enviroment

---
+++ src/test/java/br/com/kaicsantos/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kaicsantos.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
