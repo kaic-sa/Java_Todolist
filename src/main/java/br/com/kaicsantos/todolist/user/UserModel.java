d6f28c46b75d3ba568f6174d01a4a4d05312886d
kaic-sa <kaictacker@gmai.com>
2023-10-14 17:03:00 -0300

    H2 ORM added

--- src/main/java/br/com/kaicsantos/todolist/user/UserModel.java
+++ src/main/java/br/com/kaicsantos/todolist/user/UserModel.java
@@ -1,7 +1,22 @@
 package br.com.kaicsantos.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
     public  String username;
-   public  String name;
-    public String password;
+    public  String name;
+    private String password;
+
+    private LocalDateTime createdAt;
 }
